3d414cd8c638e417aa3b42f6cf4aa5fd010bf44d
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-01-29 18:31:13 +0330

    add arrow down button for scrolling album overview

--- frontend/src/productAlbum.js
+++ frontend/src/productAlbum.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
+import arrowDownIcon from './icons/arrow-down.png';
 import './productAlbum.css';
 
+function DownButton(props) {
+    return(<div className='product-album-overview-scroll-button-wrapper'>
+        <button className='product-album-overview-scroll-button'>
+            <img src={ arrowDownIcon }></img>
+        </button>
+    </div>);
+}
+
 function Overview(props) {
-    return (<div className='product-album-overview'>
-        {props.images.map((image) => {
-            return (<div className="product-album-overview-frame"> 
-            <img
-                className='product-album-overview-img'
-                src={ image.href }
-                alt={ image.rel }
-                onClick={ () => props.onClick(image.href) } />
-            </div>);
-        })}
+    const handleScroll = () => {
+        
+    }
+
+    return (<div className='product-album-overview-wrapper' >
+        <div className='product-album-overview'>
+            {props.images.map((image) => {
+                return (<div className="product-album-overview-frame"> 
+                <img
+                    className='product-album-overview-img'
+                    src={ image.href }
+                    alt={ image.rel }
+                    onClick={ () => props.onClick(image.href) } />
+                </div>);
+            })}
+            <DownButton onClick={ handleScroll } />
+        </div>
     </div>);
 }
 
