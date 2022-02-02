6b3af4bb85c40b6f9c4b44ac1af8561b56db5b72
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-02-02 18:25:12 +0330

    make dropdown a seperate component

---
+++ frontend/src/dropdown.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './dropdown.css';
+import arrowDownIcon from './icons/arrow-down.png';
+
+
+function DropDown(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedValue, setSelectedValue] = useState(props.default);
+
+    const handleDropdownClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const handleSelectedOption = (option) => {
+        props.onSelect(option);
+        setSelectedValue(option);
+    }
+
+    return (<div className="dropdown-wrapper">
+        <div tabindex="0"
+            className={ `${isOpen ? 'dropdown-hidden' : 'dropdown'}` }
+            onClick={ handleDropdownClick } onBlur={ (e) => setIsOpen(false) } >
+            <div>
+                <span>{ selectedValue }</span>
+            </div>
+            {isOpen &&
+            <div className='dropdown-option-wrapper'>
+                { props.options.map((option) => {
+                    return(<div onClick={ (e) => handleSelectedOption(option) }
+                            className="dropdown-option">
+                        <span>{ option }</span>
+                    </div>);
+                })}
+            </div>}
+            <button className='dropdown-open-button' >
+                <img src={ arrowDownIcon } />
+            </button>
+        </div>
+    </div>);
+}
+
+export default DropDown;
