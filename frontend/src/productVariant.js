8006a4efc47bf18252b1d4aaedfeb4fc25845ccd
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-02-02 13:59:15 +0330

    added dropdown menu for product variants

--- frontend/src/productVariant.js
+++ frontend/src/productVariant.js
@@ -1,15 +1,45 @@
+import React, { useState } from 'react';
+import { capitalize } from './utils';
 import { _PRODUCT_IDENTITY_KEY_ } from "./apiConstants";
 import "./productVariant.css";
+import arrowDownIcon from './icons/arrow-down.png';
 
 function DropDown(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedValue, setSelectedValue] = useState(props.options[props.default]);
+
+    const handleDropdownClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const handleSelectedOption = (option) => {
+        for(var i = 0; i < props.options.length; ++i)
+            if(props.options[i] == option) {
+                setSelectedValue(option);
+                break;
+            }
+    }
+
     return (<div className="product-variant-dropdown-wrapper">
         <span className="product-variant-header">{props.header}</span>
-        <select className="product-variant-dropdown">
-            {props.options.map((option) => {
-                console.log(option)
-                return(<option value={ option }>{ option }</option>);
-            })}
-        </select>
+        <div tabindex="0" className="product-variant-dropdown"
+            onClick={ handleDropdownClick } onBlur={ (e) => setIsOpen(false) } >
+            <div>
+                <span>{ selectedValue }</span>
+            </div>
+            {isOpen &&
+            <div className='product-variant-dropdown-option-wrapper'>
+                { props.options.map((option) => {
+                    return(<div onClick={ (e) => handleSelectedOption(option) }
+                            className="product-variant-dropdown-option">
+                        <span>{ option }</span>
+                    </div>);
+                })}
+            </div>}
+            <button className='product-variant-dropdown-open-button' >
+                <img src={ arrowDownIcon } />
+            </button>
+        </div>
     </div>);
 }
 
@@ -40,7 +70,10 @@ function ProductVariant(props) {
     return(<div className="product-variant">
         {
             getHeaders().map((header) => {
-                return(<DropDown header={ header } options={ getHeaderOptions(header) } />);
+                return(<DropDown
+                        header={ capitalize(header.replace('_', ' ')) }
+                        options={ getHeaderOptions(header) }
+                        default={ 0 } />);
             })
         }
     </div>);
