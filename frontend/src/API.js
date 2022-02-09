22136660c893f15d17942f7d268b6630012820ad
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-02-09 12:25:25 +0330

    fix CORS error by adding proxy to package.json

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -3,12 +3,12 @@ import {_API_KEY, API_URL, PRODUCTS_URL, ALSO_VIEWED_URL } from './apiConstants'
 export default {
     fetchProduct: async(productKey) => {
         const endpoint = 
-            `${ API_URL }${ PRODUCTS_URL }/${productKey}.json?apiKey=${ _API_KEY }`;
+            `/${ PRODUCTS_URL }/${productKey}.json?apiKey=${ _API_KEY }`;
         return await (await fetch(endpoint)).json();
     },
     fetchAlsoViewed: async(productKey) => {
         const endpoint =
-        `${ API_URL }${ PRODUCTS_URL }/${ productKey }/${ ALSO_VIEWED_URL }?apiKey=${ _API_KEY }`;
+        `/${ PRODUCTS_URL }/${ productKey }/${ ALSO_VIEWED_URL }?apiKey=${ _API_KEY }`;
         return await (await fetch(endpoint)).json();
     }
 };
