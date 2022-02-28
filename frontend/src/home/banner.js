21149845bffde68a7b5eb4d958c971c820b1ec88
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-03-01 00:00:30 +0330

    animation for banner

--- frontend/src/home/banner.js
+++ frontend/src/home/banner.js
@@ -1,31 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './banner.css';
+
 import poster1 from './../images/poster1.jpg';
+import poster2 from './../images/poster2.jpg';
+import poster3 from './../images/poster3.jpg';
+
 import arrowIcon from './../icons/arrow-down.png';
 
-function Button({ role }) {
+const images = [poster1, poster2, poster3];
+
+function Button({ role, handleClick }) {
     const imgStyle = {
         transform: `rotate(${role == "previous" ? 90 : 270 }deg)`
     };
+
+    const handle = () => {
+        handleClick();
+    }
 
     return (
         <div tabIndex='0' className='button-wrapper'>
             <img
              style={ imgStyle }
              className='banner-controller-previous'
-             src={ arrowIcon } />
+             src={ arrowIcon }
+             onClick={ handle }
+            />
         </div>
     );
 }
 
 
-function Banner({ images }) {
+function Banner() {
+    const [current, setCurrent] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCurrent((current + 1) % images.length);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, [current, images]);
+
+    const nextSlide = () => {
+        setCurrent((current + 1) % images.length);
+    }
+
+    const previousSlide = () => {
+        if(current - 1 < 0)
+            setCurrent(images.length - 1);
+        else
+            setCurrent(current - 1);
+    }
+
     return (
         <div className='banner'>
-            <img src={ poster1 } />
+            {
+                images.map(
+                    (image, i) => 
+                        <div 
+                          className={ `slide ${current == i ? 'active-slide' : ''}` }
+                          key={ i }
+                        >
+                            <img src={ image } />
+                        </div>)
+            }
             <div className='banner-controller'>
-                <Button role={ 'previous' } />
-                <Button role={ 'next' } />
+                <Button role={ 'previous' } handleClick={ nextSlide } />
+                <Button role={ 'next' }  handleClick={ previousSlide } />
             </div>
         </div>
     );
