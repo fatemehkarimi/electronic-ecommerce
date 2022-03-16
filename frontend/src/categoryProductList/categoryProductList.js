8fe70573629e461405b32b0ff14052b607cb280a
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-03-16 11:50:24 +0330

    continous loading of products

--- frontend/src/categoryProductList/categoryProductList.js
+++ frontend/src/categoryProductList/categoryProductList.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useCategoryProductsFetch } from '../hooks/useCategoryProductsFetch';
-import NavBar from '../navbar';
-import Grid from '../components/grid/grid';
-import Card from '../components/card/card';
-import Review from '../components/review/review';
-import { formatLongString } from './../utils';
-import PConst from '../productConstants';
-import './categoryProductList.css';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useCategoryProductsFetch } from "../hooks/useCategoryProductsFetch";
+import NavBar from "../navbar";
+import Grid from "../components/grid/grid";
+import Card from "../components/card/card";
+import Review from "../components/review/review";
+import { formatLongString } from "./../utils";
+import PConst from "../productConstants";
+import "./categoryProductList.css";
 
 function CustomCard({ product }) {
   return (
-    <Card image={ product[PConst.PRODUCT_STANDARD_IMAGE] }>
-      <span>
-        { formatLongString(product[PConst.PRODUCT_NAME]) }
-      </span>
-      <Review 
-        countReview={ product[PConst.PRODUCT_CUSTOMER_REVIEW_COUNT] }
-        value={ product[PConst.PRODUCT_CUSTOMER_REVIEW_AVERAGE] }
+    <Card image={product[PConst.PRODUCT_STANDARD_IMAGE]}>
+      <span>{formatLongString(product[PConst.PRODUCT_NAME])}</span>
+      <Review
+        countReview={product[PConst.PRODUCT_CUSTOMER_REVIEW_COUNT]}
+        value={product[PConst.PRODUCT_CUSTOMER_REVIEW_AVERAGE]}
         short="true"
       />
-      <div className='category-product-list-custom-card-price'>
-        <span className='price-symbol'>$</span>
-        <span>{ product[PConst.PRODUCT_REGULAR_PRICE] }</span>
+      <div className="category-product-list-custom-card-price">
+        <span className="price-symbol">$</span>
+        <span>{product[PConst.PRODUCT_REGULAR_PRICE]}</span>
       </div>
     </Card>
   );
@@ -30,26 +28,37 @@ function CustomCard({ product }) {
 
 function CategoryProductList() {
   const { categoryId, categoryName } = useParams();
-  const { products, totalPages, loading, error } = 
-    useCategoryProductsFetch(categoryId, 1);
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const { products, totalPages, loading, error } = useCategoryProductsFetch(
+    categoryId,
+    currentPage
+  );
+
+  const loadMore = () => {
+    setCurrentPage(currentPage + 1);
+  }
 
   return (
-    <div className='category-product-list'>
+    <div className="category-product-list">
       <NavBar />
-      <div className='category-product-list-content'>
+      <div className="category-product-list-content">
         <div className="category-product-grid-container">
           <Grid>
-          {
-          products && products.length > 0 ?
-          products.map((item) => {
-            return (
-              <a className='simple-link' href={ `/product/${item[PConst.PRODUCT_IDENTITY_KEY]}` } >
-                <CustomCard product={item} />
-              </a>
-            )
-          })
-          : <></>
-          }
+            {products && products.length > 0 ? (
+              products.map((item) => {
+                return (
+                  <a
+                    className="simple-link"
+                    href={`/product/${item[PConst.PRODUCT_IDENTITY_KEY]}`}
+                  >
+                    <CustomCard product={item} />
+                  </a>
+                );
+              })
+            ) : (
+              <></>
+            )}
+            <button onClick={ loadMore }>Load More</button>
           </Grid>
         </div>
         <div>Filter</div>
--- frontend/src/hooks/useCategoryProductsFetch.js
+++ frontend/src/hooks/useCategoryProductsFetch.js
@@ -1,52 +1,52 @@
-import { useState, useEffect } from 'react';
-import API from '../API';
-import PConst from '../productConstants';
+import { useState, useEffect } from "react";
+import API from "../API";
+import PConst from "../productConstants";
 
+export const useCategoryProductsFetch = (categoryId, pageNum = 1) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [totalPages, setTotalPages] = useState(1);
 
-export const useCategoryProductsFetch = (categoryId, pageNum=1) => {
-    const [products, setProducts] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [totalPages, setTotalPages] = useState(1);
+  useEffect(() => {
+    const fetchCategoryProducts = async (id, pageNum) => {
+      try {
+        setError(false);
+        setLoading(true);
+        const result = await API.fetchProductsOfCategory(id, pageNum);
+        if ("errorCode" in result) {
+          setError(true);
+          return;
+        }
 
+        var categoryProducts = [...products];
+        result["products"].forEach((item) => {
+          var productInfo = {};
 
-    useEffect(() => {
-        const fetchCategoryProducts = async (id, pageNum) => {
-            try {
-                setError(false);
-                setLoading(true);
-                const result = await API.fetchProductsOfCategory(id, pageNum);
-                if('errorCode' in result) {
-                    setError(true);
-                    return;
-                }
+          productInfo[PConst.PRODUCT_IDENTITY_KEY] = item["sku"];
+          productInfo[PConst.PRODUCT_CUSTOMER_REVIEW_AVERAGE] =
+            item["customerReviewAverage"];
+          productInfo[PConst.PRODUCT_CUSTOMER_REVIEW_COUNT] =
+            item["customerReviewCount"];
+          productInfo[PConst.PRODUCT_REGULAR_PRICE] = item["regularPrice"];
+          productInfo[PConst.PRODUCT_NAME] = item["name"];
+          productInfo[PConst.PRODUCT_STANDARD_IMAGE] =
+            item["images"][0]["href"];
 
-                var categoryProducts = [];
-                result['products'].forEach((item) => {
-                    var productInfo = {};
+          categoryProducts.push(productInfo);
+        });
 
-                    productInfo[PConst.PRODUCT_IDENTITY_KEY] = item['sku'];
-                    productInfo[PConst.PRODUCT_CUSTOMER_REVIEW_AVERAGE] = item['customerReviewAverage'];
-                    productInfo[PConst.PRODUCT_CUSTOMER_REVIEW_COUNT] = item['customerReviewCount'];
-                    productInfo[PConst.PRODUCT_REGULAR_PRICE] = item['regularPrice'];
-                    productInfo[PConst.PRODUCT_NAME] = item['name'];
-                    productInfo[PConst.PRODUCT_STANDARD_IMAGE] = item['images'][0]['href'];
+        setTotalPages(result["totalPages"]);
+        setProducts(categoryProducts);
+      } catch (err) {
+        setError(true);
+      }
 
-                    categoryProducts.push(productInfo);
-                });
+      setLoading(false);
+    };
 
-                setTotalPages(result['totalPages']);
-                setProducts(categoryProducts);
+    fetchCategoryProducts(categoryId, pageNum);
+  }, [categoryId, pageNum]);
 
-            }catch(err) {
-                setError(true);
-            }
-
-            setLoading(false);
-        };
-
-        fetchCategoryProducts(categoryId, pageNum);
-    }, [categoryId, pageNum]);
-
-    return { products, totalPages, loading, error };
-}
+  return { products, totalPages, loading, error };
+};
