15942ef1037d7739af5f94e88aba3c60fb663570
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-07-21 19:36:03 +0430

    fix indent in home component

--- src/home/home.js
+++ src/home/home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useMostViewedFetch } from "../hooks/useMostViewedFetch";
 import { useTrendingFetch } from '../hooks/useTrendingFetch';
+
 import NavBar from "../navbar";
 import Banner from "../components/banner/banner";
 import ProductCarousel from "../productCarousel";
+
 import './home.css';
 import './../globalStyle.css';
+
 import poster1 from '../images/poster1.jpg';
 import poster2 from '../images/poster2.jpg';
 import poster3 from '../images/poster3.jpg';
@@ -14,20 +17,20 @@ import poster5 from '../images/poster5.jpg';
 
 
 function TrendingProducts() {
-    const { products, loading, error } = useTrendingFetch();
+  const { products, loading, error } = useTrendingFetch();
 
-    return (
-        <>
-        {
-        products && products.length > 0 ?
-            <div className='home-product-carousel-wrapper'>
-                <h2>Trending products</h2>
-                <ProductCarousel products= { products } />
-            </div>
-            : undefined
-        }
-        </>
-    );
+  return (
+    <>
+    {
+      products && products.length > 0 ?
+        <div className='home-product-carousel-wrapper'>
+          <h2>Trending products</h2>
+          <ProductCarousel products= { products } />
+        </div>
+      : undefined
+    }
+    </>
+  );
 }
 
 
@@ -38,13 +41,13 @@ function MostViewedProducts() {
   return (
     <>
     {
-    products && products.length > 0 ?
-      <div className="home-product-carousel-wrapper">
+      products && products.length > 0 ?
+        <div className="home-product-carousel-wrapper">
           <h2>Most Viewed by People</h2>
           <div>
             <ProductCarousel products={ products } />
           </div>
-      </div>
+        </div>
       : undefined
     }
     </>
@@ -53,26 +56,26 @@ function MostViewedProducts() {
 
 
 function Home() {
-    const bannerImages = [poster1, poster2, poster3, poster4, poster5];
+  const bannerImages = [poster1, poster2, poster3, poster4, poster5];
 
-    return (
-        <div className="home-wrapper">
-            <div className="navbar-wrapper">
-                <NavBar />
-            </div>
-            <div className="content-wrapper">
-                <div className="banner-wrapper">
-                    <Banner images={ bannerImages } interval={ 3500 } />
-                </div>
-                <div className="home-product-recomm-wrapper">
-                    <TrendingProducts />
-                </div>
-                <div className="home-product-recomm-wrapper">
-                    <MostViewedProducts />
-                </div>
-            </div>
+  return (
+    <div className="home-wrapper">
+      <div className="navbar-wrapper">
+        <NavBar />
+      </div>
+      <div className="content-wrapper">
+        <div className="banner-wrapper">
+          <Banner images={ bannerImages } interval={ 3500 } />
         </div>
-    );
+        <div className="home-product-recomm-wrapper">
+          <TrendingProducts />
+        </div>
+        <div className="home-product-recomm-wrapper">
+          <MostViewedProducts />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
